228a1017cad36e7daacde8fd1cb8630ca909b014
Jude Kuti <judekuti@gmail.com>
2018-02-22 12:24:47 -0600

    Succeeded in building two tests

--- marsrover/test_app.py
+++ marsrover/test_app.py
@@ -3,63 +3,39 @@ from app import app
 
 
 class TestApp(unittest.TestCase):
+
+    # @classmethod
+    # def setUpClass(cls):
+    #     print('setupClass')
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #     print('teardownClass')
+
+    # def setUp(self):
+    #     print('setUp')
+    #     self.date = date
+    #     self.url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + self.date + '&api_key=G5y2M6Nxqx8ZO7E3Fr4sHOOzWTxmknVNYvOMJnZW'
+
+    # def tearDown(self):
+    #     print('tearDown\n')
     # Ensure that app was correctly set-up
     # By testing the response status logged in
+
     def test_index(self):
+        print('Test Index Page')
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     # Ensure that index page loads correctly
     # By testing some content on the page
+
     def test_index_content(self):
+        print('Test Index Page Content')
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertTrue(b'SELECT A DATE TO VIEW PHOTOS OF MARS' in response.data)
-
-    # Ensure date selection behaves correctly
-    def test_index_content(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertTrue(b'SELECT A DATE TO VIEW PHOTOS OF MARS' in response.data)
-
-    # Ensure date selection behaves correctly
-    # def setUp(self):
-    #     app.config['TESTING'] = True
-    #     self.app = app.test_client()
-
-    # def tearDown(self):
-    #     print('tearDown\n')
-
-    # def test_marsrover(self):
-    #     print('test_marsrover')
-    #     with patch('app.requests.get') as app_get:
-    #         app_get.return_value.ok = True
-    #         app.get.return_value.text = 'Success'
-
-    # response = self.test_client.post(
-    #     request_path,
-    #     data=data,
-    #     follow_redirects=False
-    # )
-    # expectedPath = '/'
-    # self.assertEqual(response.status_code, 302)
-    # self.assertEqual(urlparse(response.location).path, expectedPath)
-
-    # def test_monthly_schedule(self):
-    #     with patch('employee.requests.get') as mocked_get:
-    #         mocked_get.return_value.ok = True
-    #         mocked_get.return_value.text = 'Success'
-
-    #         schedule = self.emp_1.monthly_schedule('May')
-    #         mocked_get.assert_called_with('http://company.com/Schafer/May')
-    #         self.assertEqual(schedule, 'Success')
-
-    #         mocked_get.return_value.ok = False
-
-    #         schedule = self.emp_2.monthly_schedule('June')
-    #         mocked_get.assert_called_with('http://company.com/Smith/June')
-    #         self.assertEqual(schedule, 'Bad Response!')
 
 
 if __name__ == '__main__':
