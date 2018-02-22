bbf764c00db1f54a2e493a71e24fb5cb3790dcd6
Jude Kuti <judekuti@gmail.com>
2018-02-22 01:45:14 -0600

    fixed bug for dates with no pictures from api

--- marsrover/app.py
+++ marsrover/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import requests
 
 app = Flask(__name__)
@@ -13,16 +13,19 @@ def index():
 def marsrover():
     date = request.form['date']
     r = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + date + '&api_key=G5y2M6Nxqx8ZO7E3Fr4sHOOzWTxmknVNYvOMJnZW')
-
     j_obj = r.json()
-    image = j_obj['photos'][0]['img_src']
-    earth_date = j_obj['photos'][0]['earth_date']
-    launch_date = j_obj['photos']['rover']['launch_date']
-    landing_date = j_obj['photos']['rover']['landing_date']
-    camera = j_obj['photos'][0]['rover']['cameras']['full_name']
-    status = j_obj['photos'][0]['rover']['status']
-    rover = j_obj['photos'][0]['rover']['name']
-    return render_template('marsrover.html', image=image, earth_date=earth_date, launch_date=launch_date, landing_date=landing_date, status=status, rover=rover)
+    photos = j_obj['photos']
+    if photos != []:
+        image = j_obj['photos'][0]['img_src']
+        earth_date = j_obj['photos'][0]['earth_date']
+        launch_date = j_obj['photos'][0]['rover']['launch_date']
+        landing_date = j_obj['photos'][0]['rover']['landing_date']
+        camera = j_obj['photos'][0]['rover']['cameras'][0]['full_name']
+        status = j_obj['photos'][0]['rover']['status']
+        rover = j_obj['photos'][0]['rover']['name']
+        return render_template('marsrover.html', image=image, earth_date=earth_date, launch_date=launch_date, landing_date=landing_date, status=status, rover=rover, camera=camera)
+    else:
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
